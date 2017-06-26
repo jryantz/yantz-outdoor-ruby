968dc247a5acefa5f4097aaf9f38528cf0a29ca8
Jonathan Yantz <ijryantz@gmail.com>
2017-06-26 16:25:27 -0400

    added places variable for showing them on homepage

--- app/controllers/outdoor_controller.rb
+++ app/controllers/outdoor_controller.rb
@@ -1,4 +1,6 @@
 class OutdoorController < ApplicationController
   def index
+    # retrieve all Places ordered by state and then city, ascending
+    @places = Place.order('state asc', 'city asc')
   end
 end
