2830fb356852b385ee9871e1f85e6c088973b433
Jonathan Yantz <ijryantz@gmail.com>
2017-06-30 09:46:27 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626152228) do
+ActiveRecord::Schema.define(version: 20170630132013) do
 
   create_table "places", force: :cascade do |t|
     t.string "title"
