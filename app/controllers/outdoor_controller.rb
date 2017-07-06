c6e0b8ed4c1f32e18bd68efbf0cff20440374f27
Jonathan Yantz <ijryantz@gmail.com>
2017-07-06 12:29:53 -0400

    added dynamic sorting

--- app/controllers/outdoor_controller.rb
+++ app/controllers/outdoor_controller.rb
@@ -1,6 +1,7 @@
 class OutdoorController < ApplicationController
   def index
     # retrieve all Places ordered by state and then city, ascending
-    @places = Place.order('state asc', 'city asc')
+    # @places = Place.order('state asc', 'city asc')
+    @places = Place.sort_by(params[:order])
   end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,19 @@
 class Place < ApplicationRecord
+    scope :normal,   -> { order(state: :asc, city: :asc) }
+    scope :reverse,  -> { order(state: :desc, city: :desc) }
+    scope :alpha,    -> { order(title: :asc) }
+    scope :revalpha, -> { order(title: :desc) }
+
+    def self.sort_by(sort_param)
+      case sort_param
+      when 'reverse'
+        reverse
+      when 'alpha'
+        alpha
+      when 'revalpha'
+        revalpha
+      else
+        normal
+      end
+    end
 end
